2b849bc654336ac1ea4e05f1cf9984b45ff72030
unknown <mazayw@gmail.com>
2023-02-22 13:51:18 +0200

    feat: add business section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-
-export const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+export const Button = ({ styles }: { styles: string }) => {
+	return (
+		<button
+			type='button'
+			className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}
+		>
+			Get Started
+		</button>
+	);
+};
