eb586abc2568b5b5f62f40bf8c341b5242ed662f
unknown <mazayw@gmail.com>
2023-02-23 21:26:14 +0200

    feat: add testimonials  component

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -14,7 +14,6 @@ export const FeedbackCard = ({
 	img,
 }: FeedbackCardProps) => (
 	<div className='flex justify-between flex-col px-10 py-12 round-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card rounded-[20px]'>
-		FeedbackCard
 		<img
 			src={quotes}
 			alt='double quotes'
@@ -23,12 +22,16 @@ export const FeedbackCard = ({
 		<p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>
 			{content}
 		</p>
-    <div className='flex flex-row'>
-      <img src={img} alt={name} className='w-[48px] h-[48px] rounded-full'/>
-      <div className='flex flex-col ml-4'>
-        <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white'>{name}</h4>
-        <p className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite'>{title}</p>
-      </div>
-    </div>
+		<div className='flex flex-row'>
+			<img src={img} alt={name} className='w-[48px] h-[48px] rounded-full' />
+			<div className='flex flex-col ml-4'>
+				<h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white'>
+					{name}
+				</h4>
+				<p className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite'>
+					{title}
+				</p>
+			</div>
+		</div>
 	</div>
 );
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -7,7 +7,7 @@ export const Testimonials = () => (
 		id='clients'
 		className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
 	>
-		<div />
+		<div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient' />
 		<div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
 			<h2 className={styles.heading2}>
 				What people are <br className='sm:block hidden' /> saying about us
@@ -19,12 +19,10 @@ export const Testimonials = () => (
 				</p>
 			</div>
 		</div>
-    <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
-{feedback.map((card)=>(
-  <FeedbackCard key={card.id} {...card}/>
-))}
-
-    </div>
-
+		<div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
+			{feedback.map((card) => (
+				<FeedbackCard key={card.id} {...card} />
+			))}
+		</div>
 	</section>
 );
