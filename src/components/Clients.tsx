375b4ec5a1bcd4205ae02e3e807bacc56d85da3e
unknown <mazayw@gmail.com>
2023-02-23 21:41:36 +0200

    feat: add clients component

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { clients } from '../constants/index';
+import styles from '../style';
 
-export const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
+export const Clients = () => (
+	<section className={`${styles.flexCenter} my-4`}>
+		<div className={`${styles.flexCenter} flex-wrap w-full`}>
+			{clients.map((client) => (
+				<div
+					key={client.id}
+					className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+				>
+					<img
+						src={client.logo}
+						alt='client'
+						className='sm:w-[192px] w-[100px] object-contain hover:scale-125 duration-700'
+					/>
+				</div>
+			))}
+		</div>
+	</section>
+);
